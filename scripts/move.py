9f3099771a772e4ca2f733bb5a9767d328848a2e
DeLucca990 <pedruca2001@gmail.com>
2024-06-07 11:32:38 -0300

    Att move.py

--- scripts/move.py
+++ scripts/move.py
@@ -11,22 +11,13 @@ if not os.path.exists(dest_folder):
 
 root_folder = os.getenv('ROOT_FOLDER_MOVE')
 
-def get_unique_filename(dest_folder, folder_name):
-    new_filename = f"{folder_name}.xml"
-    while os.path.exists(os.path.join(dest_folder, new_filename)):
-        new_filename = f"{folder_name}.xml"
-    return new_filename
-
 for root, dirs, files in os.walk(root_folder):
     for file in files:
         if file.endswith('.xml'):
             src_file = os.path.join(root, file)
 
             folder_name = os.path.basename(root)
-
-            unique_filename = get_unique_filename(dest_folder, folder_name)
-
-            dest_file = os.path.join(dest_folder, unique_filename)    
+            dest_file = os.path.join(dest_folder, f"{folder_name}.xml")
 
             shutil.copy(src_file, dest_file)
             print(f'Arquivo {src_file} copiado para {dest_file}')
